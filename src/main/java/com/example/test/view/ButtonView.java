daa05c9e98d284831a8f6db45fa78ff57b6dfacb
Vladimir Bugorskiy <53443987+WMHillock@users.noreply.github.com>
2023-07-17 17:14:04 +0400

    Полировка кода, доработан класс ButtonView

--- src/main/java/com/example/test/view/ButtonView.java
+++ src/main/java/com/example/test/view/ButtonView.java
@@ -2,8 +2,6 @@ package com.example.test.view;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
@@ -13,6 +11,11 @@ import com.example.test.service.ValueService;
 @Route("/button")
 @CssImport("./styles/custom-styles.css")
 public class ButtonView extends VerticalLayout {
+
+    private final String CONSOLE = "http://localhost:8080/h2-console";
+    private final String BACK_BUTTON = "http://localhost:8080";
+    private final String TOOLTIP_H2 = "Путь к базе: jdbc:h2:mem:testdb";
+
     private Button button;
     private Button h2ConsoleButton;
     private Button backButton;
@@ -24,41 +27,51 @@ public class ButtonView extends VerticalLayout {
 
     public ButtonView() {
         setSizeFull();
-
         setJustifyContentMode(JustifyContentMode.CENTER);
         setAlignItems(Alignment.CENTER);
 
+        createIncreaseButton();
+        createH2ConsoleButton();
+        createBackButton();
+        createTextField();
+
+        add(backButton, button, textField, h2ConsoleButton);
+    }
+
+    private void createIncreaseButton() {
         button = new Button("Увеличить");
         button.addClassName("custom-button");
-
-        textField = new TextField("Значение", String.valueOf(value));
-        textField.addClassName("custom-input");
-
-        h2ConsoleButton = new Button("H2 Console");
-        h2ConsoleButton.setTooltipText("Путь к базе: jdbc:h2:mem:testdb");
-        h2ConsoleButton.addClickListener(e -> {
-            getUI().ifPresent(ui -> ui.getPage().executeJs("window.open('http://localhost:8080/h2-console');"));
-        });
-
-        backButton = new Button("Назад");
-        backButton.getStyle().set("position", "absolute");
-        backButton.getStyle().set("top", "0");
-        backButton.getStyle().set("left", "0");
-        backButton.addClickListener(e -> {
-            getUI().ifPresent(ui -> ui.getPage().setLocation("http://localhost:8080"));
-        });
-
         button.addClickListener(e -> {
             value++;
             textField.setValue(String.valueOf(value));
             valueService.setValue(value);
         });
+    }
 
+    private void createH2ConsoleButton() {
+        h2ConsoleButton = new Button("H2 Console");
+        h2ConsoleButton.setTooltipText(TOOLTIP_H2);
+        h2ConsoleButton.addClickListener(e -> {
+            getUI().ifPresent(ui -> ui.getPage().executeJs("window.open('" + CONSOLE + "');"));
+        });
+    }
+
+    private void createBackButton() {
+        backButton = new Button("Назад");
+        backButton.getStyle().set("position", "absolute");
+        backButton.getStyle().set("top", "0");
+        backButton.getStyle().set("left", "0");
+        backButton.addClickListener(e -> {
+            getUI().ifPresent(ui -> ui.getPage().setLocation(BACK_BUTTON));
+        });
+    }
+
+    private void createTextField() {
+        textField = new TextField("Значение", String.valueOf(value));
+        textField.addClassName("custom-input");
         textField.addValueChangeListener(e -> {
             value = Integer.parseInt(e.getValue());
             valueService.setValue(value);
         });
-
-        add(backButton, button, textField, h2ConsoleButton);
     }
 }
