97925b292837ced611505c35add8862b7ea4cdfc
Vladimir Bugorskiy <53443987+WMHillock@users.noreply.github.com>
2023-07-17 16:57:43 +0400

    Полировка кода, доработан класс MainView

--- src/main/java/com/example/test/view/MainView.java
+++ src/main/java/com/example/test/view/MainView.java
@@ -10,14 +10,35 @@ import com.vaadin.flow.router.Route;
 
 @Route("")
 public class MainView extends VerticalLayout {
+    private static final String IMAGE_URL = "https://raw.githubusercontent.com/WMHillock/MyWayButtonTestTask/master/src/main/resources/images/avaForTest.jpg";
+    private static final String RESUME_URL = "https://hh.ru/resume/591dc6aaff0aee5f9d0039ed1f6270384f4861";
+    private static final String TEST_BUTTON = "http://localhost:8080/button";
 
     public MainView() {
-        String imageUrl = "https://raw.githubusercontent.com/WMHillock/MyWayButtonTestTask/master/src/main/resources/images/avaForTest.jpg";
-        Image image = new Image(imageUrl, "Фото отличного Java разработчика");
+        Image image = createImage();
+        Div text = createTextDiv();
+
+        HorizontalLayout contentLayout = createContentLayout(image, text);
+        add(contentLayout);
+
+        HorizontalLayout buttonLayout = createButtonLayout();
+        add(buttonLayout);
+
+        Button testTaskButton = createTestTaskButton();
+        Button customButton = createCustomButton();
+
+        buttonLayout.add(testTaskButton, customButton);
+    }
+
+    private Image createImage() {
+        Image image = new Image(IMAGE_URL, "Фото отличного Java разработчика");
         image.setWidth("auto");
         image.setHeight("55vh");
         image.getStyle().set("margin-right", "1em");
+        return image;
+    }
 
+    private Div createTextDiv() {
         Div text = new Div();
         String formattedText = "<p>Меня зовут Владимир и я Java Разработчик</p>"
                 + "<p>На текущий момент я работал над проектами:</p>"
@@ -34,29 +55,35 @@ public class MainView extends VerticalLayout {
                 + "</ul>"
                 + "<p>Давно работаю с Java и продолжаю развиваться в данном направлении, уверен,"
                 + "что могу быть полезным в любом проекте.</p>"
-                + "<p> Прошу отправить обратную свзяь вне зависимости от вашего решения.</p>"
+                + "<p> Прошу отправить обратную связь вне зависимости от вашего решения.</p>"
                 + "<p>Спасибо за уделенное время! </p>";
         text.getElement().setProperty("innerHTML", formattedText);
+        return text;
+    }
 
+    private HorizontalLayout createContentLayout(Image image, Div text) {
         HorizontalLayout contentLayout = new HorizontalLayout(image, text);
         contentLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        return contentLayout;
+    }
 
-        add(contentLayout);
+    private HorizontalLayout createButtonLayout() {
+        return new HorizontalLayout();
+    }
 
-        HorizontalLayout buttonLayout = new HorizontalLayout();
-        add(buttonLayout);
-
+    private Button createTestTaskButton() {
         Button testTaskButton = new Button("Тестовое задание");
         testTaskButton.addClickListener(e -> {
-            getUI().ifPresent(ui -> ui.getPage().setLocation("http://localhost:8080/button"));
+            getUI().ifPresent(ui -> ui.getPage().setLocation(TEST_BUTTON));
         });
-        buttonLayout.add(testTaskButton);
+        return testTaskButton;
+    }
 
+    private Button createCustomButton() {
         Button customButton = new Button("Полное резюме");
         customButton.addClickListener(e -> {
-            getUI().ifPresent(ui -> ui.getPage().executeJs("window.open('https://hh.ru/resume/591dc6aaff0aee5f9d0039ed1f6270384f4861');"));
+            getUI().ifPresent(ui -> ui.getPage().executeJs("window.open('" + RESUME_URL + "');"));
         });
-        buttonLayout.add(customButton);
+        return customButton;
     }
 }
-
